251e44d5e05816eb109f5e017df6241e39ff377c
anastasiaov16 <anastasiaov1998@mail.ru>
2019-12-09 15:11:52 +0400

    completed

--- Epam_Tasks_OvcharovaAA/Task03/Program.cs
+++ Epam_Tasks_OvcharovaAA/Task03/Program.cs
@@ -10,11 +10,11 @@ namespace Task03
     {
         static void Main()
         {
-           // Lost.Read();
-            //var txt = WordFrequency.Read();
-           // Dictionary<string, int> result = WordFrequency.WordFrequence(txt);
-           // WordFrequency.Show(result);
-            //DynamicArray<int>.Show();
+            Lost.Read();
+            var txt = WordFrequency.Read();
+            Dictionary<string, int> result = WordFrequency.WordFrequence(txt);
+            WordFrequency.Show(result);
+            DynamicArray<int>.Show();
             var vs = new CycledDynamicArray<int>(new List<int> { 1, 2, 3, 4, 5 }, 5);
             foreach (var item in vs)
             {
