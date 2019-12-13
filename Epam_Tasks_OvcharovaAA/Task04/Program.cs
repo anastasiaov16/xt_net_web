98d04f9a583f2e8eaa98deefbdbabaa472ed50c1
anastasiaov16 <anastasiaov1998@mail.ru>
2019-12-13 16:40:53 +0400

    1-5 completed

--- Epam_Tasks_OvcharovaAA/Task04/Program.cs
+++ Epam_Tasks_OvcharovaAA/Task04/Program.cs
@@ -10,10 +10,45 @@ namespace Task04
     {
         static void Main()
         {
-            CustomSort.ShowFirstTask();
-            CustomSort.ShowSecondTask();
-            NumberArraySum.ShowFourthTask();
-            NumberArraySum.ShowFifthTask();
+            double selection;
+            do
+            {
+                Console.WriteLine("4,1 CUSTOM SORT.");
+                Console.WriteLine("4,2 CUSTOM SORT DEMO.");
+                Console.WriteLine("4,3 SORTING UNIT.");
+                Console.WriteLine("4,4 NUMBER ARRAY SUM.");
+                Console.WriteLine("4,5 TO INT OR NOT TO INT?");
+                Console.WriteLine("4,6 I SEEK YOU.");
+                Console.WriteLine("0 Exit.");
+                Console.WriteLine();
+                if (double.TryParse(Console.ReadLine(), out selection))
+                {
+                    switch (selection)
+                    {
+                        case 4.1:
+                            CustomSort.ShowFirstTask();
+                            break;
+                        case 4.2:
+                            CustomSort.ShowSecondTask();
+                            break;
+                        case 4.3:
+                            SortingUnit.Show();
+                            break;
+                        case 4.4:
+                            NumberArraySum.ShowFourthTask();
+                            break;
+                        case 4.5:
+                            NumberArraySum.ShowFifthTask();
+                            break;
+                        case 4.6:
+                            //processing...
+                            break;
+                        case 0:
+                            break;
+
+                    }
+                }
+            } while (selection != 0);
         }
     }
 }
--- Epam_Tasks_OvcharovaAA/Task04/SortingUnit.cs
+++ Epam_Tasks_OvcharovaAA/Task04/SortingUnit.cs
@@ -2,12 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task04
 {
     class SortingUnit
     {
+        private static int _threadID = 0;
+        public static event Action<string> Finish = delegate { };
+        public static void SortThread<T>(T[] array, Func<T, T, bool> comparator)
+        {
+            int threadId = ++_threadID;        
+            Thread newThread = new Thread(() => CustomSort.Sort(ref array, comparator));
+            newThread.Start();
+            newThread.Join();
+            Finish?.Invoke($"Sorting is finished in Thread with ID {threadId}.");
+        }
 
+        public static void Show()
+        {
+            int threads = 0;
+            Finish += (element) =>
+            {
+                Console.WriteLine(element);
+                threads++;
+            };
+
+            int[] tmp = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                tmp[i] = r.Next(-50, 50);
+            }
+
+            Console.WriteLine("Unsorted array:");
+            foreach (var item in tmp)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            SortThread(tmp, (m, n) => m > n);
+
+            Console.WriteLine("Sorted array:");
+            foreach (var item in tmp)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+        
     }
 }
+
